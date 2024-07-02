58dc442e823ffd33f28b7d967151971580617343
Kelvin Cortez <kelvcortez@gmail.com>
2024-07-02 15:43:39 +1200

    fix: issues

--- server/api/currency/[code].js
+++ server/api/currency/[code].js
@@ -2,9 +2,18 @@ export default defineEventHandler(async (event) => {
     
     const { code } = event.context.params
     const { currencyKey } = useRuntimeConfig()
-
+    // const { currencyKey, bearerToken } = useRuntimeConfig()
     const uri = `https://api.currencyapi.com/v3/latest?currencies=${code}&apikey=${currencyKey}`;
 
-    const { data } = await $fetch(uri)
+    const { data } = await $fetch(uri);
+
+    // Example of Post with BearerToken
+    //const { data } = await $fetch(uri, {
+    //    method: 'POST',
+    //    headers: {
+    //        'Authorization': `Bearer ${bearerToken}`
+    //    }
+    //});
+
     return data 
 })
--- server/api/ninja.js
+++ server/api/ninja.js
@@ -7,6 +7,6 @@ export default defineEventHandler(async (event) => {
     // const { age } = await readBody(event)
 
     // api call with private key
-    const { data } = await $fetch('https://api.currencyapi.com/v3/latest?apikey=cur_live_EgQVWayrOm4xon0u2YdG9Mz2w9YRtQqaPUWoa1LM')
+    const { data } = await $fetch(`https://api.currencyapi.com/v3/latest?apikey=cur_live_EgQVWayrOm4xon0u2YdG9Mz2w9YRtQqaPUWoa1LM`)
     return data
 })
