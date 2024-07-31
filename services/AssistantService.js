58ede7571996fe9bdf6907a4de4ee77077da5715
Kelvin Cortez <kelvcortez@gmail.com>
2024-07-31 12:33:58 +1200

    refac: improve caching code

--- services/AssistantService.js
+++ services/AssistantService.js
@@ -1,4 +1,3 @@
-// services/AssistantService.js
 let BASE_URL;
 
 const CACHE_KEY = 'assistants-cache';
@@ -12,6 +11,18 @@ function getBaseUrl() {
   return BASE_URL;
 }
 
+function getCachedData() {
+  const cachedData = localStorage.getItem(CACHE_KEY);
+  if (cachedData) {
+    const { data, timestamp } = JSON.parse(cachedData);
+    const now = new Date().getTime();
+    if (now - timestamp < CACHE_DURATION) {
+      return data;
+    }
+  }
+  return null;
+}
+
 function updateCache(data) {
   localStorage.setItem(CACHE_KEY, JSON.stringify({ data, timestamp: new Date().getTime() }));
 }
@@ -19,113 +30,84 @@ function updateCache(data) {
 export default {
   async getAllAssistants() {
     try {
-      // Check if the data is in localStorage and still valid
-      const cachedData = localStorage.getItem(CACHE_KEY);
+      const cachedData = getCachedData();
       if (cachedData) {
-        const { data, timestamp } = JSON.parse(cachedData);
-        const now = new Date().getTime();
-        if (now - timestamp < CACHE_DURATION) {
-          return data;
-        }
+        return cachedData;
       }
 
-      // Fetch new data if no valid cache is found
       const response = await $fetch(`${getBaseUrl()}/assistants`);
       const data = response.data;
 
-      // Store the new data in localStorage with a timestamp
       updateCache(data);
-
       return data;
     } catch (error) {
       console.error('Error fetching assistants:', error);
-      throw error; // Propagate the error back to the caller
+      throw error;
     }
   },
 
   async createAssistant(newAssistant) {
     try {
-      const response = await $fetch(`${getBaseUrl()}/assistants`, {
+      const createdAssistant = await $fetch(`${getBaseUrl()}/assistants`, {
         method: 'POST',
         body: JSON.stringify(newAssistant),
         headers: {
           'Content-Type': 'application/json'
         }
       });
-  
-      // Get the current cached data
-      const cachedData = localStorage.getItem(CACHE_KEY);
+
+      const cachedData = getCachedData();
       if (cachedData) {
-        const { data } = JSON.parse(cachedData);
-  
-         // Add the newly created assistant to the beginning of the cached data
-        const updatedData = [response, ...data];
-  
-        // Update the cache with the new data
+        const updatedData = [createdAssistant, ...cachedData];
         updateCache(updatedData);
-  
-        console.log('Cache updated with new assistant:', updatedData); // Debugging statement
       } else {
-        // If no cache exists, create it
-        updateCache([response]);
+        updateCache([createdAssistant]);
       }
-      return response; // Assuming response.data contains the newly created assistant
+
+      return createdAssistant;
     } catch (error) {
       console.error('Error creating assistant:', error);
-      throw error; // Propagate the error back to the caller
+      throw error;
     }
   },
 
   async updateAssistant(id, updatedAssistant) {
     try {
-      const response = await $fetch(`${getBaseUrl()}/assistants/${id}`, {
+      const updatedAssistantData = await $fetch(`${getBaseUrl()}/assistants/${id}`, {
         method: 'PUT',
         body: JSON.stringify(updatedAssistant),
         headers: {
           'Content-Type': 'application/json'
         }
       });
-      
-      // Get the current cached data
-      const cachedData = localStorage.getItem(CACHE_KEY);
+
+      const cachedData = getCachedData();
       if (cachedData) {
-        const { data } = JSON.parse(cachedData);
-
-        // Update the assistant in the cached data
-        const updatedData = data.map(assistant =>
-          assistant.id === id ? response : assistant
+        const updatedData = cachedData.map(assistant =>
+          assistant.id === id ? updatedAssistantData : assistant
         );
-
-        // Update the cache with the new data
         updateCache(updatedData);
       }
 
-      return response; // Assuming response.data contains the updated assistant
+      return updatedAssistantData;
     } catch (error) {
       console.error(`Error updating assistant with ID ${id}:`, error);
-      throw error; // Propagate the error back to the caller
+      throw error;
     }
   },
 
   async deleteAssistant(id) {
     try {
-      // Make the delete request
       await $fetch(`${getBaseUrl()}/assistants/${id}`, { method: 'DELETE' });
 
-      // Get the current cached data
-      const cachedData = localStorage.getItem(CACHE_KEY);
+      const cachedData = getCachedData();
       if (cachedData) {
-        const { data } = JSON.parse(cachedData);
-
-        // Remove the deleted assistant from the cached data
-        const updatedData = data.filter(assistant => assistant.id !== id);
-
-        // Update the cache with the new data
+        const updatedData = cachedData.filter(assistant => assistant.id !== id);
         updateCache(updatedData);
       }
     } catch (error) {
       console.error('Error deleting assistant:', error);
-      throw error; // Propagate the error back to the caller
+      throw error;
     }
   }
 };
--- services/VectorService.js
+++ services/VectorService.js
@@ -1,9 +1,8 @@
-// services/AssistantService.js
 let BASE_URL;
 
 const VECTOR_STORES_CACHE_KEY = 'vector-store-cache';
 const VECTOR_FILES_CACHE_KEY = 'vector-file-cache';
-const CACHE_DURATION = 24 * 60 * 60 * 1000; // 24 hours'
+const CACHE_DURATION = 24 * 60 * 60 * 1000; // 24 hours
 
 function getBaseUrl() {
   if (!BASE_URL) {
@@ -13,25 +12,42 @@ function getBaseUrl() {
   return BASE_URL;
 }
 
+function getCachedData(key) {
+  const cachedData = localStorage.getItem(key);
+  if (cachedData) {
+    const { data, timestamp } = JSON.parse(cachedData);
+    const now = new Date().getTime();
+    if (now - timestamp < CACHE_DURATION) {
+      return data;
+    }
+  }
+  return null;
+}
+
+function updateCache(key, data) {
+  if (data) {
+    localStorage.setItem(key, JSON.stringify({ data, timestamp: new Date().getTime() }));
+  } else {
+    console.error('Attempted to update cache with invalid data:', data);
+  }
+}
 
 export default {
   async listVectorStores() {
     try {
-      // Check if the data is in localStorage and still valid
-      const cachedData = localStorage.getItem(VECTOR_STORES_CACHE_KEY);
+      // Check if the data is in cache and still valid
+      const cachedData = getCachedData(VECTOR_STORES_CACHE_KEY);
       if (cachedData) {
-        const { data, timestamp } = JSON.parse(cachedData);
-        const now = new Date().getTime();
-        if (now - timestamp < CACHE_DURATION) {
-          return data;
-        }
+        return cachedData;
       }
 
+      // Fetch new data if no valid cache is found
       const response = await $fetch(`${getBaseUrl()}/vector_stores`);
       const data = response.message;
 
       // Store the new data in localStorage with a timestamp
-      localStorage.setItem(VECTOR_STORES_CACHE_KEY, JSON.stringify({ data, timestamp: new Date().getTime() }));
+      updateCache(VECTOR_STORES_CACHE_KEY, data);
+
       return data;
     } catch (error) {
       console.error('Error fetching vector stores:', error);
@@ -41,20 +57,18 @@ export default {
 
   async listVectorFiles() {
     try {
-      // Check if the data is in localStorage and still valid
-      const cachedData = localStorage.getItem(VECTOR_FILES_CACHE_KEY);
+      // Check if the data is in cache and still valid
+      const cachedData = getCachedData(VECTOR_FILES_CACHE_KEY);
       if (cachedData) {
-        const { data, timestamp } = JSON.parse(cachedData);
-        const now = new Date().getTime();
-        if (now - timestamp < CACHE_DURATION) {
-          return data;
-        }
+        return cachedData;
       }
 
+      // Fetch new data if no valid cache is found
       const data = await $fetch(`${getBaseUrl()}/files`);
 
       // Store the new data in localStorage with a timestamp
-      localStorage.setItem(VECTOR_FILES_CACHE_KEY, JSON.stringify({ data, timestamp: new Date().getTime() }));
+      updateCache(VECTOR_FILES_CACHE_KEY, data);
+
       return data;
     } catch (error) {
       console.error('Error fetching vector files:', error);
